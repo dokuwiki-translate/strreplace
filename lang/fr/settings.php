a62195a8a3e4870de527a7b8bc49698f917fb209
Leboucq <thibault@capitalvision.fr>
2014-01-22 15:50:25 +0100

    translation update

---
+++ lang/fr/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Leboucq <thibault@capitalvision.fr>
+ */
+$lang['search_1']              = 'Chercher le terme 1:';
+$lang['search_2']              = 'Chercher le terme 2:';
+$lang['search_3']              = 'Chercher le terme 3:';
+$lang['search_4']              = 'Chercher le terme 4:';
+$lang['replace_1']             = 'Remplacement 1:';
+$lang['replace_2']             = 'Remplacement 2:';
+$lang['replace_3']             = 'Remplacement 3:';
+$lang['replace_4']             = 'Remplacement 4:';
