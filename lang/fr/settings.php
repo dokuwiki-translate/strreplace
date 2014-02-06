e896c36bd27d2cd899cbc297c49c900db7b3fb7f
Schplurtz le Déboulonné <schplurtz@laposte.net>
2014-02-06 02:05:04 +0100

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -4,11 +4,14 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Leboucq <thibault@capitalvision.fr>
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  */
-$lang['search_1']              = 'Chercher le terme 1:';
-$lang['search_2']              = 'Chercher le terme 2:';
-$lang['search_3']              = 'Chercher le terme 3:';
-$lang['search_4']              = 'Chercher le terme 4:';
+$lang['do_replace']            = 'Pour effectuer une recherche et remplacement, activer cette option. Pour chaque terme de recherche saisissez un texte de remplacement à la même position. Si cette option est désactivée après avoir été active, tous les fichiers seront de nouveau traités. Voir <a href=\'https://www.dokuwiki.org/plugin:strreplace#usage\'>Utilisation</a> (en anglais)<br /><br />';
+$lang['suspend']               = 'Suspendre temporairement la recherche et remplacement. Lorsque cette option sera désactivée, les fichiers précédemment traités ne le seront pas à nouveau.';
+$lang['search_1']              = 'Terme de recherche 1 :';
+$lang['search_2']              = 'Terme de recherche 2 :';
+$lang['search_3']              = 'Terme de recherche 3 :';
+$lang['search_4']              = 'Terme de recherche 4 :';
 $lang['replace_1']             = 'Remplacement 1:';
 $lang['replace_2']             = 'Remplacement 2:';
 $lang['replace_3']             = 'Remplacement 3:';
