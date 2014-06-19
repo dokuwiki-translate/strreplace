4ba3a8d1ba433b176ee789f90d29aef6f48ee92b
Dennis Denk <dennis@elementcraft.de>
2014-06-20 00:00:38 +0200

    translation update

---
+++ lang/de/settings.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Dennis Denk <dennis@elementcraft.de>
+ */
+$lang['do_replace']            = 'Um suchen und ersetzten auszuführen, setzen sie diese Option auf wahr. Geben sie für jeden Suchbegriff einen Ersatz im entsprechend nummerierten Feld an. Wenn es auf falsch gesetzt ist werden alle Dateien neu durchgeführt. Beispiel:  <a href=\'https://www.dokuwiki.org/plugin:strreplace#usage\'>Usage</a><br /><br />';
+$lang['search_1']              = 'Suchbegriff 1:';
+$lang['search_2']              = 'Suchbegriff 2:';
+$lang['search_3']              = 'Suchbegriff 3:';
+$lang['search_4']              = 'Suchbegriff 4:';
+$lang['replace_1']             = 'Ersatz 1:';
+$lang['replace_2']             = 'Ersatz 2:';
+$lang['replace_3']             = 'Ersatz 3:';
+$lang['replace_4']             = 'Ersatz 4:';
