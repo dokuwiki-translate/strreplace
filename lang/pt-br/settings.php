58db27a371285823fc3648304e2ed402b20218aa
Alexandre Belchior <alexbelchior@gmail.com>
2018-07-05 14:00:32 +0200

    translation update

---
+++ lang/pt-br/settings.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
+ */
+$lang['do_replace']            = 'Para pesquisar e substituir, defina essa opção como true. Para cada termo de pesquisa, insira uma substituição na mesma posição numerada. Se definido como falso depois que o strreplace estiver ativo, todos os arquivos serão reprocessados. Veja: <a href=\'https://www.dokuwiki.org/plugin:strreplace#usage\'> Uso </a> <br /> <br />';
+$lang['suspend']               = 'Suspender temporariamente a pesquisa e substituir o processamento; quando redefinido para arquivos falsos anteriormente processados não são reprocessados.';
+$lang['search_1']              = 'Termo de pesquisa 1:';
+$lang['search_2']              = 'Termo de pesquisa 2:';
+$lang['search_3']              = 'Termo de pesquisa 3:';
+$lang['search_4']              = 'Termo de pesquisa 4:';
+$lang['replace_1']             = 'Substituição 1:';
+$lang['replace_2']             = 'Substituição 2:';
+$lang['replace_3']             = 'Substituição 3:';
+$lang['replace_4']             = 'Substituição 4:';
