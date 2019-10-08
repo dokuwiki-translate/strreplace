a68cc6cabe71f117c11d24ba4ba958c504a1a831
Bartek S <sadupl@gmail.com>
2019-10-08 20:10:48 +0200

    translation update

---
+++ lang/pl/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Bartek S <sadupl@gmail.com>
+ */
+$lang['search_1']              = 'Szukany termin 1:';
+$lang['search_2']              = 'Szukany termin 2:';
+$lang['search_3']              = 'Szukany termin 3:';
+$lang['search_4']              = 'Szukany termin 4:';
+$lang['replace_1']             = 'Zamiennik 1:';
+$lang['replace_2']             = 'Zamiennik 2:';
+$lang['replace_3']             = 'Zamiennik 3:';
+$lang['replace_4']             = 'Zamiennik 4:';
