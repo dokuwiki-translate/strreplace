3f1a3bb93c14d23ef153067d719031f0c4a3742d
Gordon <moeller.gordon@googlemail.com>
2019-12-17 09:00:26 +0100

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -2,10 +2,12 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Gordon <moeller.gordon@googlemail.com>
  * @author Dennis Denk <dennis@elementcraft.de>
  */
 $lang['do_replace']            = 'Um suchen und ersetzten auszuführen, setzen sie diese Option auf wahr. Geben sie für jeden Suchbegriff einen Ersatz im entsprechend nummerierten Feld an. Wenn es auf falsch gesetzt ist werden alle Dateien neu durchgeführt. Beispiel:  <a href=\'https://www.dokuwiki.org/plugin:strreplace#usage\'>Usage</a><br /><br />';
+$lang['suspend']               = 'Vorübergehend die Suche und das Ersetzen aussetzen. Wenn der Wert auf "falsch" gesetzt wird, werden zuvor durchsuchte Dateien nicht noch einmal durchsucht.';
 $lang['search_1']              = 'Suchbegriff 1:';
 $lang['search_2']              = 'Suchbegriff 2:';
 $lang['search_3']              = 'Suchbegriff 3:';
